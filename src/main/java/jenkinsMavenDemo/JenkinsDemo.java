e21bb2f223a9c9a4a97e3f8ebaa0274f0e5b3af1
lariy <lariy@HP>
2024-03-22 04:40:47 +0530

    first commit

---
+++ src/main/java/jenkinsMavenDemo/JenkinsDemo.java
@@ -0,0 +1,11 @@
+package jenkinsMavenDemo;
+
+public class JenkinsDemo {
+
+	public static void main(String[] args) {
+	  System.out.println("Jenkins Demo");
+	  System.out.println("Welcome Jenkins");
+
+	}
+
+}
